41b691991299af156c5148fdbb3bb2aa5ca13a82
niyonsaba <niyonsabas003@gmail.com>
2026-02-04 12:19:03 +0200

    monday tasks all solutions

--- monday.js
+++ monday.js
@@ -1,121 +1,113 @@
 // // # MONDAY – Variables, Data & Simple Logic
+//1. Print the sum of a = 5 and b = 3
+let a = 5;
+let b = 3;
+console.log(a + b);
 
-// //1. Print the sum of a = 5 and b = 3
-// let a = 5;
-// let b = 3;
-// console.log(a + b);
+//2. Store a user's age and print it
+let age = 22;
+console.log(age);
 
-// //2. Store a user's age and print it
-// let age = 22;
-// console.log(age);
+//3. Swap values of two variables
+let x = 10;
+let y = 20;
 
-// //3. Swap values of two variables
-// let x = 10;
-// let y = 20;
+let temp = x;
+x = y;
+y = temp;
 
-// let temp = x;
-// x = y;
-// y = temp;
+console.log(x, y);
 
-// console.log(x, y);
+//4. Array of 3 prices and print the first one
+let prices = [100, 200, 300];
+console.log(prices[0]);
 
-// //4. Array of 3 prices and print the first one
-// let prices = [100, 200, 300];
-// console.log(prices[0]);
+//5. Object for a product (name & price)
+let product = {
+  name: "Phone",
+  price: 500
+};
 
-// //5. Object for a product (name & price)
-// let product = {
-//   name: "Phone",
-//   price: 500
-// };
+console.log(product);
 
-// console.log(product);
+//6. Predict the output
+let x;
+console.log(x + 5);
 
-// //6. Predict the output
-// let x;
-// console.log(x + 5);
+//7. Check if a number is greater than 10
+let num = 15;
 
-// //7. Check if a number is greater than 10
-// let num = 15;
+if (num > 10) {
+  console.log("Greater than 10");
+}
 
-// if (num > 10) {
-//   console.log("Greater than 10");
-// }
+//8. Variable for light ON or OFF
+let isLightOn = true;
 
-// //8. Variable for light ON or OFF
-// let isLightOn = true;
+//9. Sentence as a string variable
+let sentence = "JavaScript is easy to learn";
 
-// //9. Sentence as a string variable
-// let sentence = "JavaScript is easy to learn";
+//10. Add two numbers stored in variables
+let a = 7;
+let b = 8;
 
-// //10. Add two numbers stored in variables
-// let a = 7;
-// let b = 8;
+console.log(a + b);
 
-// console.log(a + b);
+//11. Predict output
+let x = "5";
+let y = 2;
+console.log(x + y);
 
-// //11. Predict output
-// let x = "5";
-// let y = 2;
-// console.log(x + y);
+//12. Calculate total of 3 numbers
+let a = 5;
+let b = 10;
+let c = 15;
 
+console.log(a + b + c);
 
-// Output:
+//13. Store today’s day as a string
+let today = "Monday";
 
-// "52"
+//14. Check if two numbers are equal
+let a = 10;
+let b = 10;
 
+if (a === b) {
+  console.log("Equal");
+}
 
-//  String + number = string concatenation
+//15. Variable that stores a student score
+let score = 75;
 
-// //12. Calculate total of 3 numbers
-// let a = 5;
-// let b = 10;
-// let c = 15;
-
-// console.log(a + b + c);
-
-// //13. Store today’s day as a string
-// let today = "Monday";
-
-// //14. Check if two numbers are equal
-// let a = 10;
-// let b = 10;
-
-// if (a === b) {
-//   console.log("Equal");
-// }
-
-// //15. Variable that stores a student score
-// let score = 75;
-
-// //16. Predict output
-// let score = 80;
-// score = score + 5;
-// console.log(score);
+//16. Predict output
+let score = 80;
+score = score + 5;
+console.log(score);
 
 
-// Output:
+Output:
 
-// 85
+85
 
-// //17. Print "Pass" if score > 50
-// let score = 60;
+//17. Print "Pass" if score > 50
+let score = 60;
 
-// if (score > 50) {
-//   console.log("Pass");
-// }
+if (score > 50) {
+  console.log("Pass");
+}
 
-// //18. Subtract two numbers
-// let a = 20;
-// let b = 7;
+//18. Subtract two numbers
+let a = 20;
+let b = 7;
 
-// console.log(a - b);
+console.log(a - b);
 
-// //19. Multiply two numbers
+//19. Multiply two numbers
 let a = 4;
 let b = 6;
 
 console.log(a * b);
 
-// // 20. Explain in one sentence what a variable is.
-// A variable is a container used to store data that can be used and changed in a program.
+//20. What is a variable? (one sentence)
+
+ A variable is a container used to store data that can be used and changed in a program.
